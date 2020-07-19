b48644599f8610b15a9b287ca46ae98505f76453
nnqq <43930873+nnqq@users.noreply.github.com>
2020-07-20 01:02:49 +0300

    wip

--- main.go
+++ main.go
@@ -14,43 +14,39 @@ import (
 	"time"
 )
 
-func cleanup() {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+func cleanup(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
+
+	e := func(err error) {
+		if err != nil {
+			logger.Log.Error().Err(err).Send()
+		}
+	}
 
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		err := stan.Conn.Close()
-		if err != nil {
-			logger.Log.Error().Err(err).Send()
-		}
+		e(stan.Conn.Close())
 	}()
 
 	go func() {
 		defer wg.Done()
-		err := mongo.DB.Client().Disconnect(ctx)
-		if err != nil {
-			logger.Log.Error().Err(err).Send()
-		}
+		e(mongo.DB.Client().Disconnect(ctx))
 	}()
 	wg.Wait()
 }
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	done := make(chan struct{}, 1)
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
 		<-signals
+		cleanup(ctx)
 		cancel()
-		cleanup()
-		done <- struct{}{}
 	}()
 
-	go producer.URL(ctx, config.Env.FilePath)
-
-	<-done
+	producer.URL(ctx, config.Env.FilePath)
 }
